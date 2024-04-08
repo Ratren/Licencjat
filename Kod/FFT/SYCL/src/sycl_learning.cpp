5ecfd7a2df19f7d31f78dd014686f72354ed3511
Ratren <rafa.lenart12@gmail.com>
2024-04-08 04:03:55 +0200

    fft baza i próba SYCL

---
+++ Kod/FFT/SYCL/src/sycl_learning.cpp
@@ -0,0 +1,144 @@
+#include <CL/sycl.hpp>
+#include <algorithm>
+#include <cmath>
+#include <complex>
+#include <cstring>
+#include <iostream>
+#include <iterator>
+#include <random>
+
+using namespace cl::sycl;
+
+#define J Complex(0.0, 1.0)
+
+typedef std::complex<double> Complex;
+
+void generate_bit_reversal_table(unsigned long num_bits,
+                                 unsigned long *bit_reversal_table) {
+  unsigned long tableSize = 1 << num_bits;
+  for (unsigned long i = 0; i < tableSize; ++i) {
+    unsigned long reversed = 0;
+    for (unsigned long j = 0; j < num_bits; ++j) {
+      if (i & (1 << j)) {
+        reversed |= (1 << (num_bits - 1 - j));
+      }
+    }
+    bit_reversal_table[i] = reversed;
+  }
+}
+
+Complex *generate_input_array(unsigned long size) {
+  Complex *input_array = new Complex[size];
+  std::random_device rd;
+  std::mt19937 gen(rd());
+  std::uniform_real_distribution<double> dis(0.0, 1.0);
+
+  for (unsigned long i = 0; i < size; ++i) {
+    double real_part = dis(gen);
+    input_array[i] = Complex(real_part, 0);
+  }
+
+  return input_array;
+}
+//
+//                    [0, 1, 2, 3, 4, 5, 6, 7]
+//                [0, 2, 4, 6]        [1, 3, 5, 7]
+//              [0, 4]    [2, 6]    [1, 5]    [3, 7]
+//
+//
+Complex *fft(Complex *input_array, unsigned long size) {
+
+  Complex *output_array = new Complex[size];
+  //  std::copy(input_array, input_array + size, output_array);
+
+  queue Q{cpu_selector{}};
+
+  Complex *unified_memory_array = malloc_shared<Complex>(size, Q);
+  std::copy(input_array, input_array + size, unified_memory_array);
+
+  long num_bits = static_cast<long>(std::log2(size));
+  unsigned long *bit_reversal_table = new unsigned long[1 << num_bits];
+  generate_bit_reversal_table(num_bits, bit_reversal_table);
+
+  for (unsigned long step = 1; step <= num_bits; ++step) {
+    unsigned long step_size = 1 << step;
+    Complex omega =
+        std::pow(std::exp(1), (2 * M_PI * J) / static_cast<double>(step_size));
+
+    for (unsigned long start = 0; start < size; start += step_size) {
+      //      for (unsigned long i = 0; i < step_size / 2; i++) {
+      //        unsigned long index_even = bit_reversal_table[start + i];
+      //        unsigned long index_odd = bit_reversal_table[start + i +
+      //        step_size / 2]; Complex temp = output_array[index_even];
+      //        output_array[index_even] = output_array[index_even] +
+      //                                   std::pow(omega, i) *
+      //                                   output_array[index_odd];
+      //        output_array[index_odd] =
+      //            temp - std::pow(omega, i) * output_array[index_odd];
+      //      }
+      Q.parallel_for(range<1>(step_size / 2), [=](id<1> i) {
+         unsigned long index_even = bit_reversal_table[start + i];
+         unsigned long index_odd =
+             bit_reversal_table[start + i + step_size / 2];
+         Complex temp = unified_memory_array[index_even];
+         unified_memory_array[index_even] =
+             unified_memory_array[index_even] +
+             std::pow(omega, i) * unified_memory_array[index_odd];
+         unified_memory_array[index_odd] =
+             temp - std::pow(omega, i) * unified_memory_array[index_odd];
+       }).wait();
+    }
+  }
+
+  //for (long i = 0; i < size; i++) {
+  //  std::cout << std::abs(unified_memory_array[bit_reversal_table[i]]) << "; ";
+  //}
+  //std::cout << '\n';
+
+  std::cout << "done \n";
+  delete[] bit_reversal_table;
+  free(unified_memory_array, Q);
+
+  return output_array;
+}
+
+int main() {
+  unsigned long input_size = std::pow(2, 20);
+
+  Complex *input_array = generate_input_array(input_size);
+
+  //  Complex *input_array = new Complex[16];
+  //  input_array[0] = static_cast<Complex>(0);
+  //  input_array[1] = static_cast<Complex>(1);
+  //  input_array[2] = static_cast<Complex>(2);
+  //  input_array[3] = static_cast<Complex>(3);
+  //  input_array[4] = static_cast<Complex>(4);
+  //  input_array[5] = static_cast<Complex>(5);
+  //  input_array[6] = static_cast<Complex>(6);
+  //  input_array[7] = static_cast<Complex>(7);
+  //  input_array[8] = static_cast<Complex>(8);
+  //  input_array[9] = static_cast<Complex>(9);
+  //  input_array[10] = static_cast<Complex>(10);
+  //  input_array[11] = static_cast<Complex>(11);
+  //  input_array[12] = static_cast<Complex>(12);
+  //  input_array[13] = static_cast<Complex>(13);
+  //  input_array[14] = static_cast<Complex>(14);
+  //  input_array[15] = static_cast<Complex>(15);
+
+  Complex *output_array = fft(input_array, input_size);
+
+  //  for (long i = 0; i < input_size; i++) {
+  //    std::cout << input_array[i] << '\t';
+  //  }
+  //  std::cout << '\n';
+
+  long num_bits = static_cast<long>(std::log2(input_size));
+  unsigned long *bit_reversal_table = new unsigned long[1 << num_bits];
+  generate_bit_reversal_table(num_bits, bit_reversal_table);
+
+
+  delete[] input_array;
+  delete[] bit_reversal_table;
+
+  return 0;
+}
